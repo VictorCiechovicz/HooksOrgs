a9000d75abd7206117a8048fa454963894fc42be
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-08-14 12:31:16 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Text, SafeAreaView} from 'react-native';
+
+import Home from './src/Pages/Home';
+
+export default function App() {
+  return (
+    <SafeAreaView>
+      <Home />;
+    </SafeAreaView>
+  );
+}
---
+++ src/Pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Topo from '../../Componente/Topo'
+
+export default function Home(){
+  return(
+
+<Topo />
+  )
+}
+
+
