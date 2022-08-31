0ba9c9efe2cc76f58c644a16249ac5bf32282f34
victor ciechovicz <victor09ciechovicz@outlook.com>
2022-08-30 23:18:50 -0300

    up

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import {SafeAreaView, StyleSheet} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
 
 import Home from './src/Pages/Home';
 import MelhoresProdutores from './src/Componente/MelhoreProdutores';
 
 const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
@@ -16,11 +20,12 @@ export default function App() {
       <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen
-            name="Melhores Produtores"
-            component={MelhoresProdutores}
-          />
+          <Tab.Screen name="Melhores Produtores"component={MelhoresProdutores}/>
         </Tab.Navigator>
+        <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+     
+    </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
